a011591665bac44dc3d0838e0b978da2d10454f1
Unibrighter <rocklee.au@foxmail.com>
2015-08-29 12:03:56 +1000

    set up the basic frame , get rid of /data folder

--- src/rocklee/methods/Approach.java
+++ src/rocklee/methods/Approach.java
@@ -1,6 +1,55 @@
 package rocklee.methods;
 
+import org.apache.log4j.Logger;
+
+
+/***
+ * This Class uses some Algorithm to do the comparisons between the String
+ * and then outputs some results according to the strategy we use.
+ * 
+ * 
+ * @version 2015-08-29 11:54
+ * @author Kunliang WU
+ *
+ */
+
 public class Approach
 {
 
+	/*
+	 * Use the global edit distance strategy to compute the match rate
+	 * 
+	 * */
+	
+	public static double globalEditDistance(String word1 ,String word2)
+	{
+		double result=0d;
+		
+		//TODO implement the algorithm
+				
+		return result;
+	}
+	
+	
+	/*
+	 * Use the local edit distance strategy to compute the match rate
+	 * 
+	 * Note this is to find whether the query exists in the given aim Text
+	 * 
+	 * Instead of compare the texts to calculate the result
+	 * */
+	public static double localEditDistance(String query ,String aimText)
+	{
+		double result=0d;
+		
+		//TODO implement the algorithm
+				
+		return result;
+	}
+	
+	
+	
+	
+	
+	
 }
